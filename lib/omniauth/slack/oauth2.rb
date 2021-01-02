8f09a0222d34affdff52a8428bb6a77e1260ecf9
Omar Johnson <github@omarvelous.com>
2021-01-02 01:40:11 -0500

    init

---
+++ lib/omniauth/slack/oauth2.rb
@@ -0,0 +1,10 @@
+require "omniauth/slack/oauth2/version"
+
+module Omniauth
+  module Slack
+    module Oauth2
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/omniauth/slack/oauth2/version.rb
@@ -0,0 +1,7 @@
+module Omniauth
+  module Slack
+    module Oauth2
+      VERSION = "0.1.0"
+    end
+  end
+end
